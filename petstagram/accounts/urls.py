664363ed7dcae13ff6c219bb6c028ab9935a9652
Tsvetan Eftenov <105812168+eftenow@users.noreply.github.com>
2023-05-28 21:15:05 +0300

    added templates and 404 page

--- petstagram/accounts/urls.py
+++ petstagram/accounts/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 from petstagram.accounts.views import login_account, delete_account, details_account, edit_account, register_account
 
 urlpatterns = [
-    path('login/', login_account, name='account register'),
+    path('login/', login_account, name='login account'),
     path('register/', register_account, name='register account'),
     path('profile/<int:pk>/', include([
         path('', details_account, name='account details'),
--- petstagram/settings.py
+++ petstagram/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-5cimo%-5pu#jm@*welxivu5814pt4komuya+6^storxiqfi795'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -122,6 +122,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR/'staticfiles'
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
